59355b655e5b1f626131239f0f25e2ab96409c66
s.holtkamp <s.holtkamp@conterra.de>
2022-11-15 08:53:17 +0100

    Update project to 4.14.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
-/*
- * Copyright (C) 2021 con terra GmbH (info@conterra.de)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 const gulp = require("gulp");
 const mapapps = require('ct-mapapps-gulp-js');
+const isProduction = process.env.NODE_ENV === "production";
+console.info(`Configuring gulp build for ${isProduction ? "production" : "development"}`);
 
 mapapps.registerTasks({
     /* A detailed description of available setting is available at https://www.npmjs.com/package/ct-mapapps-gulp-js */
+    compress: isProduction,
+
+    /* build source maps as e.g. ".js.map" */
+    sourceMaps: "file",
+
     /* a list of themes inside this project */
     themes: [/*"sample-theme"*/],
     /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
